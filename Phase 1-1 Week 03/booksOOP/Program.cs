0956634a4d95ad267ca582933c6d77fb83f1f774
chantebryana <chantebryana@gmail.com>
2022-04-19 11:17:03 -0500

    ph1.1wk2; oop classes books; flesh out more elments of book array

--- Phase 1-1 Week 03/booksOOP/Program.cs
+++ Phase 1-1 Week 03/booksOOP/Program.cs
@@ -6,13 +6,6 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
-			//set local variables
-			string title = "Old Jules";
-			string author = "Mari Sandoz";
-			int yearRead = 2022;
-			string notes = "This book is a biography of Mari's father and of western Nebraska from the 1880s through the 1930s. As many reviewers state, this is an unromanticized depiction of both her father and the land. A highly-recommended read for anyone from Nebraska, interested in pioneer history, or appreciative of good writing.";
-
-			
 			//declare values; create two-dimensional book array
 			int arrSize = 10;
 			Book[] bookCollection = new Book[arrSize];
@@ -23,10 +16,10 @@ namespace MyApplication
 			}
 
 			//set first book!
-			bookCollection[0].Title = title;
-			bookCollection[0].Author = author;
-			bookCollection[0].YearRead = yearRead;
-			bookCollection[0].Notes = notes;
+			bookCollection[0].Title = "Old Jules";
+			bookCollection[0].Author = "Mari Sandoz";
+			bookCollection[0].YearRead = 2022;
+			bookCollection[0].Notes = "This book is a biography of Mari's father and of western Nebraska from the 1880s through the 1930s. As many reviewers state, this is an unromanticized depiction of both her father and the land. A highly-recommended read for anyone from Nebraska, interested in pioneer history, or appreciative of good writing.";
 
 			//set second book
 			bookCollection[1].Title = "Foundation";
@@ -34,8 +27,24 @@ namespace MyApplication
 			bookCollection[1].YearRead = 2021;
 			bookCollection[1].Notes = "Asimov's simple prose and story lines employ some of my favorite scifi tropes: using technological advances as a lens to explore the human condition.";
 
-			Console.WriteLine(bookCollection[0]);
-			Console.WriteLine(bookCollection[1]);
+			//set third book
+			bookCollection[3].Title = "Piranesi";
+			bookCollection[3].Author = "Susanna Clarke";
+			bookCollection[3].YearRead = 2021;
+			bookCollection[3].Notes = "Donna's bookclub read; read during Schneider reunion; creative rpemise and satisfying build-up / resolution.";
+
+			//set fourth book
+			bookCollection[6].Title = "The Gigantic Beard That Was Evil";
+			bookCollection[6].Author = "Stephen Collins";
+			bookCollection[6].YearRead = 2020;
+			bookCollection[6].Notes = "Xmas gift to Jim; Graphic Novel; So fun and good and creative and thoughtful.";
+
+			//print to page if element contains book object with info
+			for (int i = 0; i < bookCollection.Length; i++) {
+				if (bookCollection[i].YearRead != -1) {
+					Console.WriteLine(bookCollection[i]);
+				}
+			}
 
     } //end Main
   } //end class Program

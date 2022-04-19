c8a066d0af81b108684964e1934248d46928de61
chantebryana <chantebryana@gmail.com>
2022-04-19 11:10:35 -0500

    p1.1wk2; oop classes books; set objs to array

--- Phase 1-1 Week 03/booksOOP/Book.cs
+++ Phase 1-1 Week 03/booksOOP/Book.cs
@@ -20,5 +20,26 @@ namespace MyApplication
 		public string Notes
 		{ get; set; }
 
+		//default constructor
+		public Book () {
+			Title = "";
+			Author = "";
+			YearRead = -1;
+			Notes = "";
+		}
+
+		//overridden constructor with arguments
+		public Book (string newTitle, string newAuthor, int newYearRead, string newNotes) {
+			Title = newTitle;
+			Author = newAuthor;
+			YearRead = newYearRead;
+			Notes = newNotes;
+		}
+
+		//adapt tostring for console wl
+		public override string ToString() {
+			return $"    Read {Title} by {Author} in {YearRead}\n    Thoughts: {Notes}\n";
+		}
+
   } //end class book
 } //end namespace myapp
--- Phase 1-1 Week 03/booksOOP/Program.cs
+++ Phase 1-1 Week 03/booksOOP/Program.cs
@@ -6,19 +6,36 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
-			//instantiate Book object
-			Book bookCollection = new Book();
+			//set local variables
+			string title = "Old Jules";
+			string author = "Mari Sandoz";
+			int yearRead = 2022;
+			string notes = "This book is a biography of Mari's father and of western Nebraska from the 1880s through the 1930s. As many reviewers state, this is an unromanticized depiction of both her father and the land. A highly-recommended read for anyone from Nebraska, interested in pioneer history, or appreciative of good writing.";
 
-			//set variables
-			bookCollection.Title = "Old Jules";
-			bookCollection.Author = "Mari Sandoz";
-			bookCollection.YearRead = 2022;
-			bookCollection.Notes = "This book is a biography of Mari's father and of western Nebraska from the 1880s through the 1930s. As many reviewers state, this is an unromanticized depiction of both her father and the land. A highly-recommended read for anyone from Nebraska, interested in pioneer history, or appreciative of good writing.";
+			
+			//declare values; create two-dimensional book array
+			int arrSize = 10;
+			Book[] bookCollection = new Book[arrSize];
 
-			Console.WriteLine($"Title: {bookCollection.Title}");
-			Console.WriteLine($"Author: {bookCollection.Author}");
-			Console.WriteLine($"Year Read: {bookCollection.YearRead}");
-			Console.WriteLine($"Notes: {bookCollection.Notes}");
+			//instantiate Book object for each array element
+			for (int i = 0; i < bookCollection.Length; i++) {
+					bookCollection[i] = new Book();
+			}
+
+			//set first book!
+			bookCollection[0].Title = title;
+			bookCollection[0].Author = author;
+			bookCollection[0].YearRead = yearRead;
+			bookCollection[0].Notes = notes;
+
+			//set second book
+			bookCollection[1].Title = "Foundation";
+			bookCollection[1].Author = "Isaac Asimov";
+			bookCollection[1].YearRead = 2021;
+			bookCollection[1].Notes = "Asimov's simple prose and story lines employ some of my favorite scifi tropes: using technological advances as a lens to explore the human condition.";
+
+			Console.WriteLine(bookCollection[0]);
+			Console.WriteLine(bookCollection[1]);
 
     } //end Main
   } //end class Program

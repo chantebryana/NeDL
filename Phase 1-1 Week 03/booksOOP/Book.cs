fc5b304e1746b985db0f6e798321d25d29f5ce15
chantebryana <chantebryana@gmail.com>
2022-04-19 09:26:32 -0500

    ph1.1wk2; oop classes; book collection; 1st mvp: functional classes and print to console

---
+++ Phase 1-1 Week 03/booksOOP/Book.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MyApplication
+{
+  class Book
+  {
+    //Title property
+		public string Title 
+    { get; set; }  
+
+		//Author property
+		public string Author
+		{ get; set; }
+
+		//Year Read property
+		public int YearRead
+		{ get; set; }
+
+		//Notes property
+		public string Notes
+		{ get; set; }
+
+  } //end class book
+} //end namespace myapp
--- Phase 1-1 Week 03/booksOOP/Program.cs
+++ Phase 1-1 Week 03/booksOOP/Program.cs
@@ -6,7 +6,20 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
-			
+			//instantiate Book object
+			Book bookCollection = new Book();
+
+			//set variables
+			bookCollection.Title = "Old Jules";
+			bookCollection.Author = "Mari Sandoz";
+			bookCollection.YearRead = 2022;
+			bookCollection.Notes = "This book is a biography of Mari's father and of western Nebraska from the 1880s through the 1930s. As many reviewers state, this is an unromanticized depiction of both her father and the land. A highly-recommended read for anyone from Nebraska, interested in pioneer history, or appreciative of good writing.";
+
+			Console.WriteLine($"Title: {bookCollection.Title}");
+			Console.WriteLine($"Author: {bookCollection.Author}");
+			Console.WriteLine($"Year Read: {bookCollection.YearRead}");
+			Console.WriteLine($"Notes: {bookCollection.Notes}");
+
     } //end Main
   } //end class Program
 } //end namespace MyApp

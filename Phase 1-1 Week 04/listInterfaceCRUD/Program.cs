e75911c8cbc690ec3a3de40ccb3bb036e5d31ac5
chantebryana <chantebryana@gmail.com>
2022-04-26 13:52:08 -0500

    Phase1-1Wk04 OOP Abstract Interface; Program file; add new Employee object to list

--- Phase 1-1 Week 04/listInterfaceCRUD/Program.cs
+++ Phase 1-1 Week 04/listInterfaceCRUD/Program.cs
@@ -29,6 +29,7 @@ namespace listInterfaceCRUD
 
 			//=============================================================
 			//experiment with reading (finding) and employee(s) in the list
+			//=============================================================
 
 			//talk to human and save response
 			Console.WriteLine("Enter an employee Last Name to find: ");
@@ -51,6 +52,50 @@ namespace listInterfaceCRUD
 			if (!(found)) {
 				Console.WriteLine("Oops! Name not found.");
 			}
+
+			//=============================================================
+			//experiment with creating (adding) an employee to the list
+			//=============================================================
+
+			//talk to human and save response
+			Console.WriteLine("Enter an employee last name to add:");
+			string lName = Console.ReadLine();
+			Console.WriteLine("Enter an employee first name to add:");
+			string fName = Console.ReadLine();
+			Console.WriteLine("Enter an employee type to add (S or H):");
+			string eType = (Console.ReadLine()).ToUpper(); //convert user input to uppercase
+
+			//conditional actions depending on employee type
+			switch (eType) {
+				case "S": 
+					//continue talking to human and save response
+					Console.WriteLine("Enter annual salary:");
+					double aSalary = Convert.ToDouble(Console.ReadLine());
+
+					//add new user input to list & let user know
+					employeeList.Add(new SalaryEmployee(lName, fName, eType, aSalary));
+					Console.WriteLine("Salary employee added. Printing out new list.");
+					break;
+				case "H": 
+					//continue talking to human and save response
+					Console.WriteLine("Enter hourly rate:");
+					double hRate = Convert.ToDouble(Console.ReadLine());
+
+					//add new user input to list & let user know
+					employeeList.Add(new SalaryEmployee(lName, fName, eType, hRate));
+					Console.WriteLine("Hourly employee added. Printing out new list.");
+
+					break;
+				default: 
+					Console.WriteLine("Oops! Wrong option and you only get one chance here. Nothing was added to list.");
+					break;
+			} //end switch
+
+			//print list again
+			foreach (Employee anEmployee in employeeList) {
+				Console.WriteLine(anEmployee);
+			}
+
 
     } //end main
   } //end class

1f7ebeb548cd222384ea217637b65101a93b777d
chantebryana <chantebryana@gmail.com>
2022-04-28 14:46:45 -0500

    phase 1-1 wk 4 competency; basic menu for bank acct mgmt

--- Phase 1-1 Week 04/competency04/Program.cs
+++ Phase 1-1 Week 04/competency04/Program.cs
@@ -5,50 +5,85 @@ namespace bankAcctActions
 {
 	class Program
   {
+		//print option menu and return user input
+		static string askUserMenuOptions () {
+			//print menu options
+			Console.WriteLine("");
+			Console.WriteLine("Select a Menu Option:");
+			Console.WriteLine("   L: List all accounts.");
+			Console.WriteLine("   D: Perform Deposit transaction.");
+			Console.WriteLine("   W: Perform Withdrawal transaction.");
+			Console.WriteLine("   Q: Quit the program.");
+
+			//return user input, converted to upper-case
+			return Console.ReadLine().ToUpper();
+		}
     static void Main(string[] args)
     {
 			//create list of bank accounts
 			List<BankAcct> bankAccounts = new List<BankAcct>();
 
+			//add series of hard-coded savings, checking, and cd accounts
 			bankAccounts.Add(new Savings(1000, "Savings", 1000.00, 5.5));
+			bankAccounts.Add(new Savings(1001, "Savings", 95000.00, 3.95));
 			bankAccounts.Add(new Checking(2001, "Checking", 500.00, 0.67));
 			bankAccounts.Add(new Checking(2002, "Checking", 75.00, 1.59));
 			bankAccounts.Add(new CD(3001, "CD", 5000.00, 2.0, 25.00));
 			bankAccounts.Add(new CD(3002, "CD", 7500.50, 2.0, 49.00));
+
+			//declare variables for later
+			bool userChoice;
+			string userChoiceString;
 			
-			foreach(BankAcct anAcct in bankAccounts) {
-				Console.WriteLine(anAcct);
-			}
+			//repeat till user quits
+			do {
+				//get valid user input
+				do {
+					//initialize variable
+					userChoice = false;
 
-			//
-			//DEPOSIT
-			//
-			int acctNumber = 1000;
-			foreach(BankAcct anAcct in bankAccounts) {
-				if (anAcct.AcctId == acctNumber) {
-					anAcct.AcctBalance = anAcct.Deposit(500.00);
+					//provide the user a list of menu options
+					//get valid user input
+					userChoiceString = askUserMenuOptions();
+
+					//valid user choices
+					userChoice = (userChoiceString == "L" || 
+												userChoiceString == "D" || 
+												userChoiceString == "W" || 
+												userChoiceString == "Q"
+					);
+					//if user enters invalid choice, repeat till correct
+					if (!userChoice) {
+						Console.WriteLine("Oops! Please enter a valid menu option");
+					}
+				} while (!userChoice) ; //end inner do
+
+				//logic for individual user choices
+				//L - LIST ALL ACCTS
+				if (userChoiceString == "L") {
+					Console.WriteLine("LIST ACCOUNTS");
 				}
-			}
 
-			Console.WriteLine("\nDEPOSIT");
-			foreach(BankAcct anAcct in bankAccounts) {
-				Console.WriteLine(anAcct);
-			}
+				//D - DEPOSIT
+				else if (userChoiceString == "D") {
+					Console.WriteLine("DEPOSIT");
 
-			//
-			//WITHDRAWAL
-			//
-			acctNumber = 3002;
-			foreach(BankAcct anAcct in bankAccounts) {
-				if (anAcct.AcctId == acctNumber) {
-					anAcct.AcctBalance = anAcct.Withdrawal(7452.00);
 				}
-			}
 
-			Console.WriteLine("\nWITHDRAWAL");
-			foreach(BankAcct anAcct in bankAccounts) {
-				Console.WriteLine(anAcct);
-			}
+				//W - WITHDRAWAL
+				else if (userChoiceString == "W") {
+					Console.WriteLine("WITHDRAWAL");
+
+				}
+
+				//Q - QUIT - COMPLETE
+				else {
+					Console.WriteLine("QUIT PROGRAM");
+					Console.WriteLine("~~~Adios Sayōnara Goodbye Chao Mae Alsalama~~~");
+				} //end if/else
+
+			} while (!(userChoiceString == "Q")) ; //end outer do
+
 
     } //end main
   } //end class

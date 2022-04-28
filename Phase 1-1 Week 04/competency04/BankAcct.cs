af05dcc5720635789629450aa5b1131b527076ce
chantebryana <chantebryana@gmail.com>
2022-04-28 11:52:14 -0500

    Phase1-1Wk04Competency; Bank Acct; build parent class and successfully test in Program file

---
+++ Phase 1-1 Week 04/competency04/BankAcct.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic; //needed for lists
+
+namespace bankAcctActions
+{
+	abstract class BankAcct
+  {
+		//properties
+		public int AcctId
+		{ get; set; }
+
+		public string AcctType
+		{ get; set; }
+
+		public double AcctBalance
+		{ get; set; }
+
+		//default constructor
+		public BankAcct() {
+			AcctId = -1;
+			AcctType = "";
+			AcctBalance = 0D;
+		}
+
+		//override constructor
+		public BankAcct(int newId, string newType, double newBalance) {
+			AcctId = newId;
+			AcctType = newType;
+			AcctBalance = newBalance;
+		}
+
+		//deposit method: same logic for all types of accounts
+		//if depositAmount > 0, add to currentBalance; return currentBalance
+		public virtual double Deposit(double depositAmount) {
+			if (depositAmount > 0) {
+				AcctBalance = AcctBalance + depositAmount;
+			} 
+			return AcctBalance;
+		}
+
+		//override ToString to write to console
+		public override string ToString() {
+			return $"\nAccount Id: {AcctId}\nAccount Type: {AcctType}\nCurrent Balance: ${AcctBalance}";
+		}
+
+  } //end class
+} //end namespace
---
+++ Phase 1-1 Week 04/competency04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic; //needed for lists
+
+namespace bankAcctActions
+{
+	class Program
+  {
+    static void Main(string[] args)
+    {
+			//create list of bank accounts
+			List<BankAcct> bankAccounts = new List<BankAcct>();
+
+			bankAccounts.Add(new BankAcct(1000, "Savings", 1000.00));
+			
+			foreach(BankAcct anAcct in bankAccounts) {
+				Console.WriteLine(anAcct);
+			}
+
+			foreach(BankAcct anAcct in bankAccounts) {
+				anAcct.AcctBalance = anAcct.Deposit(500.00);
+			}
+
+			foreach(BankAcct anAcct in bankAccounts) {
+				Console.WriteLine(anAcct);
+			}
+
+    } //end main
+  } //end class
+} //end namespace

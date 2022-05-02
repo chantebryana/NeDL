2f1fc5a73e12007b0ff09034b2751113059082b8
chantebryana <chantebryana@gmail.com>
2022-05-02 10:18:14 -0500

    wk5 competency; abstract Membership class

--- Phase 1-1 Week 05/competency05/Membership.cs
+++ Phase 1-1 Week 05/competency05/Membership.cs
@@ -4,6 +4,59 @@ namespace CustomerMemeberships
 {
 	abstract class Membership
   {
+		//properties
+		public int MembershipId
+		{ get; set; }
+
+		public string Email
+		{ get; set; }
+
+		public string MembershipType
+		{ get; set; }
+
+		public double AnnualCost
+		{ get; set; }
+
+		public double MonthlyPurchaseTotal
+		{ get; set; }
+
+		//default constructor
+		public Membership() {
+			MembershipId = -1;
+			Email = "";
+			MembershipType = "";
+			AnnualCost = -1D;
+			MonthlyPurchaseTotal = -1D;
+		}
+
+		//override constructor
+		public Membership(int newId, string newEmail, string newType, double newCost, double newTotal) {
+			MembershipId = newId;
+			Email = newEmail;
+			MembershipType = newType;
+			AnnualCost = newCost;
+			MonthlyPurchaseTotal = newTotal;
+		}
+
+		//methods!
+		//purchase method: same logic for all types of memberships
+		public double Purchase(double purchaseAmount) {
+			return MonthlyPurchaseTotal += purchaseAmount;
+		}
+
+		//return method: same logic for all types of memberships
+		public double Return(double returnAmount) {
+			return MonthlyPurchaseTotal -= returnAmount;
+		}
+
+		//abstract apply cashback rewards: different logic depending on which membership type
+		public abstract double ApplyCashbackReward();
+
+
+		//override tostring (for pretty printening)
+		public override string ToString() {
+			return $"\nMembership Id: {MembershipId}\nEmail Address: {Email}\nMembership Type: {MembershipType}\nAnnual Cost: ${AnnualCost}\nMonthly Purchase Total: ${MonthlyPurchaseTotal}";
+		}
 
   } //end class
 } //end namespace
--- Phase 1-1 Week 05/competency05/Program.cs
+++ Phase 1-1 Week 05/competency05/Program.cs
@@ -7,7 +7,18 @@ namespace CustomerMemeberships
   {
     static void Main(string[] args)
     {
-			Console.WriteLine("Hello world");
+			//create list of memberships
+			List<Membership> memberships = new List<Membership>();
+
+			//add a series of hard-coded membership types
+			memberships.Add(new Membership(1, "email@domain.com", "Regular", 100.00, 500.00));
+			memberships.Add(new Membership(2, "email2@domain.com", "Executive", 200.00, 1000.00));
+			memberships.Add(new Membership(3, "email3@domain.com", "Nonprofit", 50.00, 250.00));
+			memberships.Add(new Membership(4, "email4@domain.com", "Corporate", 274.99, 25000.00));
+
+			foreach(Membership aMembership in memberships) {
+						Console.WriteLine(aMembership);
+					}
 
     } //end main
   } //end class

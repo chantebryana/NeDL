feecfac2efa50d5c470d1a3ddf79628183605029
chantebryana <chantebryana@gmail.com>
2022-05-10 13:34:32 -0500

    halfway through setting state

--- React/ticTacToe/tic-tac-toe/src/index.js
+++ React/ticTacToe/tic-tac-toe/src/index.js
@@ -5,20 +5,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null,
-		};
-	}
-
-  render() {
+	render() {
     return (
       <button 
 				className="square" 
-				onClick={() => this.setState({value: 'X'})}
+				onClick={() => this.props.onClick()}
 			>
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -33,7 +26,12 @@ class Board extends React.Component {
   }
   
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+       />
+    );
   }
 
   render() {

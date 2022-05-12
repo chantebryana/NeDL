7f1d6a22a187d1589f6ba7381242f3c504a26915
chantebryana <chantebryana@gmail.com>
2022-05-12 14:08:47 -0500

    successfully implement DELETE MVP

--- phase 1-2/wk01/competencyWk1/Competency.js
+++ phase 1-2/wk01/competencyWk1/Competency.js
@@ -8,18 +8,56 @@ function validateANDadd() {
 		return false;
 	} 
 	//passes validation
-	//check if string is palindrome and
 	//add the word to the correct column
 	else {
-		//CE ADD METHOD
-
-		//print word on first column and report whether it's a palindrome
+		//print word on ToDo column
 		var tableRef = document.getElementById("myList1");
-			(tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;
+		(tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;
 			
-		//erase the form fields
+		//erase the form field
 		document.forms["myForm"]["newWord"].value = "";
 		return true;
+	}
+}
+
+function validateANDdelete() {
+	//get new word from form
+	var wordToDelete = document.forms["myForm"]["newWord"].value;
+	console.log("wordToDelete: " + wordToDelete);
+
+	//validation
+	//check if word is empty string
+	if (wordToDelete == "") {
+		alert("Oops! Your string's empty. Try again.");
+		return false;
+	}
+	//passes validation
+	//see if new word matches any in list
+	else {
+		//initialize variables
+		let matchFound = false;
+		var tableRef = document.getElementById("myList1");
+		
+		//loop thru todo list
+		for (let i = 0; i < tableRef.rows.length; i++) {
+			//if match is found
+			if ((tableRef.rows[i].innerHTML).toLowerCase() == (wordToDelete).toLowerCase()) {
+				//flag to true
+				matchFound = true;
+				
+				//remove matching element from list
+				console.log("Match! " + tableRef.rows[i].innerHTML);
+				tableRef.rows[i].remove();
+			}
+		}
+
+		//alert if no match found
+		if (matchFound == false) {
+			alert("Oops! No match found in list. Try again.");
+		}
+		
+		//remove word from form field
+		document.forms["myForm"]["newWord"].value = "";
 	}
 }
 

0396c526b8e8b4ab202e9f8f074a37606e1ddd3d
chantebryana <chantebryana@gmail.com>
2022-05-18 15:10:49 -0500

    create feature component https://angular.io/tutorial/toh-pt3

--- phase 1-2/wk02/angular/angular-tour-of-heroes/src/app/mock-heroes.ts
+++ phase 1-2/wk02/angular/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -5,7 +5,7 @@ import { Hero } from './hero';
 export const HEROES: Hero[] = [
 	{ id: 11, name: 'Thich Nhat Hanh' },
 	{ id: 12, name: 'Carl Sagan' },
-	{ id: 13, name: 'Carolin Girvan' },
+	{ id: 13, name: 'Caroline Girvan' },
 	{ id: 14, name: '14th Dalai Lama' },
 	{ id: 15, name: 'Ida B Wells' },
 	{ id: 16, name: 'Jane Goodall' },

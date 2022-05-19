99371ca600c8a6a5cb6d38fdbfcdff4b1ae4ddde
chantebryana <chantebryana@gmail.com>
2022-05-19 16:10:17 -0500

    hard code components, just to get something on the page

--- phase 1-2/wk02/competencyReact/nasa-apod-react/src/index.js
+++ phase 1-2/wk02/competencyReact/nasa-apod-react/src/index.js
@@ -7,24 +7,21 @@ console.log('Hello World!');
 class ImageInfo extends React.Component {
   render() {
     return (
-      <button className="imageInfo">
-        {/* TODO */}
-      </button>
+			<div>
+				<p>Title: Gravity's Grin</p>
+				<p><a href="https://apod.nasa.gov/apod/image/2205/cheshirecat_chandra_complg.jpg">HD Image Link</a></p>
+				<br />
+				<p><em>Albert Einstein's general theory of relativity, published over 100 years ago, predicted the phenomenon of gravitational lensing. And that's what gives these distant galaxies such a whimsical appearance, seen through the looking glass of X-ray and optical image data from the Chandra and Hubble space telescopes. Nicknamed the Cheshire Cat galaxy group, the group's two large elliptical galaxies are suggestively framed by arcs. The arcs are optical images of distant background galaxies lensed by the foreground group's total distribution of gravitational mass. Of course, that gravitational mass is dominated by dark matter. The two large elliptical \"eye\" galaxies represent the brightest members of their own galaxy groups which are merging. Their relative collisional speed of nearly 1,350 kilometers/second heats gas to millions of degrees producing the X-ray glow shown in purple hues. Curiouser about galaxy group mergers? The Cheshire Cat group grins in the constellation Ursa Major, some 4.6 billion light-years away.</em></p>
+			</div>
     );
   }
 }
 
 class NasaImage extends React.Component {
-  renderSquare(i) {
-    return <ImageInfo />;
-  }
-
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
+				<img src="https://apod.nasa.gov/apod/image/2205/cheshirecat_chandra_complg_1024.jpg"></img>
       </div>
     );
   }
@@ -33,7 +30,13 @@ class NasaImage extends React.Component {
 class DateSelector extends React.Component {
 	render () {
 		return (
-			<div></div>
+			<div>
+				<form name="myForm">
+					Select date to display: 
+					<input type="date" name="apod" max="2022-05-23"></input>
+					<input type="button" value="Search for Image" onClick={() => {console.log("clicky stuffs")}}></input>
+				</form>
+			</div>
 		);
 	}
 }
@@ -42,11 +45,14 @@ class NasaApod extends React.Component {
   render() {
     return (
       <div className="nasa-apod">
-        <div className="date-selector">
+        <h1>&#127756; NASA Astronomy Picture of the Day &#127756;</h1>
+				<div className="date-selector">
 					<DateSelector />
+					<br /><br />
 				</div>
 				<div className="nasa-image">
           <NasaImage />
+					<br /><br />
         </div>
         <div className="image-info">
 					<ImageInfo />

f540fc93b601dfcbd7ad47927fe1e62c3ce5eebe
chantebryana <chantebryana@gmail.com>
2022-05-24 13:45:40 -0500

    practicing MVC http://congeritc.blogspot.com/2013/01/mvc-example-with-c-console-program.html

---
+++ phase 1-2/wk03/TipMVC/TipMVC/TipCalcController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipMVC
+{
+    internal class TipCalcController
+    {
+        ///
+
+        ///The TipCalController class brings together
+        ///the display and the tip or model classes.
+        ///I use the constructor to instantiate the Display.
+        ///Instantiating the Display calls its constructor
+        ///which calls the Get input method
+        ///Once the input is entered I can instantiate
+        ///the Tip class and pass the values from the
+        ///Dipslay class. Notice the dot notation and observe
+        ///how the two classes interact.
+
+        ///
+
+        private TipModel tipModel;
+        private TipView tipView;
+
+        public TipCalcController()
+        {
+            tipView = new TipView();
+            tipModel = new TipModel(tipView.Amt, tipView.Percentage);
+            tipView.TipAmount = tipModel.CalculateTip();
+            tipView.Total = tipModel.CalculateTotal();
+            tipView.ShowTipandTotal();
+        }
+    }
+}

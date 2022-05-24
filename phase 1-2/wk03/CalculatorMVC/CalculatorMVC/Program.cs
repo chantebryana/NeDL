0cd8f56cba159f7914ef700e5a0a0022b97b75be
chantebryana <chantebryana@gmail.com>
2022-05-24 15:19:27 -0500

    initial attempt at looping app; not the final answer

--- phase 1-2/wk03/CalculatorMVC/CalculatorMVC/Program.cs
+++ phase 1-2/wk03/CalculatorMVC/CalculatorMVC/Program.cs
@@ -10,7 +10,20 @@ namespace CalculatorMVC
     {
         static void Main(string[] args)
         {
-            CalcController controller = new CalcController();
+            bool endApp = false;
+
+            while (!endApp)
+            {
+                CalcController controller = new CalcController();
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            
+            
         }
 
     }

b444526c514d9a1864e9f10632655094ad5bb74a
chantebryana <chantebryana@gmail.com>
2022-05-25 15:14:27 -0500

    create and implement cloud logger

---
+++ phase 1-2/wk03/DepInj01/DepInj01/CloudLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepInj01
+{
+    internal class CloudLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Inside Log method of CloudLogger.");
+            LogToCloud(message);
+        }
+
+        //private method; use in DatabaseLogger
+        private void LogToCloud(string message)
+        {
+            Console.WriteLine("Method: LogToCloud, Text {0}", message);
+        }
+    }
+}
--- phase 1-2/wk03/DepInj01/DepInj01/Program.cs
+++ phase 1-2/wk03/DepInj01/DepInj01/Program.cs
@@ -10,9 +10,10 @@ namespace DepInj01
     {
         static void Main(string[] args)
         {
-            //instantiate new filelogger and databaselogger
+            //instantiate new filelogger and databaselogger and cloudlogger
             ILogger logger = new FileLogger();
             ILogger logger2 = new DatabaseLogger();
+            ILogger logger3 = new CloudLogger();
             
             //pass new filelogger-type logger variable into 
             //instantiation of ProductService class
@@ -21,11 +22,17 @@ namespace DepInj01
             //database version of productService
             ProductService productServiceD = new ProductService(logger2);
 
+            //cloud version
+            ProductService productServiceC = new ProductService(logger3);
+
             //log to text file
             productService.Log("Chante testing final result");
 
             //log to db
             productServiceD.Log("load my database");
+
+            //log to cloud
+            productServiceC.Log("Cloudy!!!!!");
         }
     }
 }

fdff1ab7ca26df5ea7bb7c81fca38821801347c7
chantebryana <chantebryana@gmail.com>
2022-05-25 13:44:58 -0500

    digest the code I've written and annotate

--- phase 1-2/wk03/DepInj01/DepInj01/FileLogger.cs
+++ phase 1-2/wk03/DepInj01/DepInj01/FileLogger.cs
@@ -8,11 +8,18 @@ namespace DepInj01
 {
     internal class FileLogger
     {
+        //no variables or properties to implement
+        //no constructors
+        //go straight to building methods
+        
+        //method 1
         public void Log(string message)
         {
             Console.WriteLine("Inside Log method of FileLogger");
             LogToFile(message);
         }
+        
+        //method 2
         private void LogToFile(string message)
         {
             Console.WriteLine("Method: LogToFile, Text: {0}", message);
--- phase 1-2/wk03/DepInj01/DepInj01/ProductService.cs
+++ phase 1-2/wk03/DepInj01/DepInj01/ProductService.cs
@@ -8,7 +8,14 @@ namespace DepInj01
 {
     internal class ProductService
     {
+        //ProductService doesn't inherit on FileLogger, 
+        //but does instantiate a local FileLogger element
+        //PS is therefore dependent of FL
+
+        //instantiate instance of FileLogger
         private readonly FileLogger _fileLogger = new FileLogger();
+
+        //Log method; calls Log method inside FileLogger class
         public void Log(string message)
         {
             _fileLogger.Log(message);

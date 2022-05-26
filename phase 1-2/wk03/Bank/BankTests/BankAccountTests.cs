614c29911c2c630dd0d745426d20ed5a627b66aa
chantebryana <chantebryana@gmail.com>
2022-05-26 14:20:09 -0500

    run tests, fix code, rerun tests, everything passes

--- phase 1-2/wk03/Bank/BankTests/BankAccountTests.cs
+++ phase 1-2/wk03/Bank/BankTests/BankAccountTests.cs
@@ -7,8 +7,26 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Debit_WithValidAmount_UpdatesBalance()
         {
+            //Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            
+            //sets up new BankAccount object with a beginning balance
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            //Act
+            //withdrawal a valid amount from BankAccount object
+            account.Debit(debitAmount);
+
+            //Assert
+            double actual = account.Balance;
+
+            //user Assert.AreEqual() method to verify that the ending balance is as expected
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+
         }
     }
 }

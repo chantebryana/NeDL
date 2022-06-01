e51ed2f96262000b29e47581fd91c23f0873228a
chantebryana <chantebryana@gmail.com>
2022-06-01 13:07:29 -0500

    finish https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio

---
+++ phase 1-2/wk04/TodoApiCe/TodoApiCe/Models/TodoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApiCe.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ phase 1-2/wk04/TodoApiCe/TodoApiCe/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApiCe.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}

557a9b675966332e1aa82a896cdce60d085d3865
chantebryana <chantebryana@gmail.com>
2022-06-06 14:01:44 -0500

    implement in-memory database

---
+++ phase 1-2/wk05/GardenApi/GardenApi/Models/GardenContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GardenApi.Models
+{
+    public class GardenContext : DbContext
+    {
+        public GardenContext(DbContextOptions<GardenContext> options) :base(options)
+        {
+        }
+
+        public DbSet<GardenItem> GardenItems { get; set; }
+    }
+}
--- phase 1-2/wk05/GardenApi/GardenApi/Startup.cs
+++ phase 1-2/wk05/GardenApi/GardenApi/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GardenApi.Models;
 
 namespace GardenApi
 {
@@ -28,10 +22,7 @@ namespace GardenApi
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GardenApi", Version = "v1" });
-            });
+            services.AddDbContext<GardenContext>(opt => opt.UseInMemoryDatabase("GardenList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +31,6 @@ namespace GardenApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GardenApi v1"));
             }
 
             app.UseHttpsRedirection();

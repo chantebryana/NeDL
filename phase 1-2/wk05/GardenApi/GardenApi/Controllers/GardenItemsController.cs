38682c8e8d453b308490da3ad19935c48213d3e0
chantebryana <chantebryana@gmail.com>
2022-06-06 15:34:49 -0500

    finish backend setup (restful api)

---
+++ phase 1-2/wk05/GardenApi/GardenApi/Controllers/GardenItemsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GardenApi.Models;
+
+namespace GardenApi.Controllers
+{
+    [Route("api/GardenItems")]
+    [ApiController]
+    public class GardenItemsController : ControllerBase
+    {
+        private readonly GardenContext _context;
+
+        public GardenItemsController(GardenContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/GardenItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GardenItem>>> GetGardenItems()
+        {
+            return await _context.GardenItems.ToListAsync();
+        }
+
+        // GET: api/GardenItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GardenItem>> GetGardenItem(long id)
+        {
+            var gardenItem = await _context.GardenItems.FindAsync(id);
+
+            if (gardenItem == null)
+            {
+                return NotFound();
+            }
+
+            return gardenItem;
+        }
+
+        // PUT: api/GardenItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGardenItem(long id, GardenItem gardenItem)
+        {
+            if (id != gardenItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(gardenItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GardenItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/GardenItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<GardenItem>> PostGardenItem(GardenItem gardenItem)
+        {
+            _context.GardenItems.Add(gardenItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetGardenItem), new { id = gardenItem.Id }, gardenItem);
+        }
+
+        // DELETE: api/GardenItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGardenItem(long id)
+        {
+            var gardenItem = await _context.GardenItems.FindAsync(id);
+            if (gardenItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.GardenItems.Remove(gardenItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GardenItemExists(long id)
+        {
+            return _context.GardenItems.Any(e => e.Id == id);
+        }
+    }
+}
--- phase 1-2/wk05/GardenApi/GardenApi/Models/GardenItem.cs
+++ phase 1-2/wk05/GardenApi/GardenApi/Models/GardenItem.cs
@@ -5,6 +5,6 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsSowed { get; set; }
-        public string HarvestDate { get; set; }
+        public string HarvestNotes { get; set; }
     }
 }

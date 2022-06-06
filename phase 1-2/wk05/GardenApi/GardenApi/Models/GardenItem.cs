70fc158648edb41b0c3c2c4fe8380d1847123ed6
chantebryana <chantebryana@gmail.com>
2022-06-06 13:56:14 -0500

    create Models folder and file

---
+++ phase 1-2/wk05/GardenApi/GardenApi/Models/GardenItem.cs
@@ -0,0 +1,10 @@
+﻿namespace GardenApi.Models
+{
+    public class GardenItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSowed { get; set; }
+        public string HarvestDate { get; set; }
+    }
+}

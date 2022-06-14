f132e975d3a5fa81e1b39ef7eefb51f787b7537d
chantebryana <chantebryana@gmail.com>
2022-06-14 14:22:00 -0500

    fix a problem: add return statement to savePicture() (aka learn how to read a tutorial)

--- phase 1-3/wk01/photo-gallery/src/app/services/photo.service.ts
+++ phase 1-3/wk01/photo-gallery/src/app/services/photo.service.ts
@@ -10,14 +10,54 @@ export class PhotoService {
   //define variables
   public photos: UserPhoto[] = []; //array contains reference to each photo captured w camera
 
+  //save photo to filesystem
+  //pass in photo object, which represents the newly captured image
+  private async savePicture(photo: Photo) {
+    //convert photo to base64 format, required by filesystem api to save
+    const base64Data = await this.readAsBase64(photo);
+
+    //write the file to the new directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  //helper function readAsBase64
+  //requires small amount of platform-specific logic (web v mobile)
+  private async readAsBase64(photo: Photo) {
+    //fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
   constructor() { }
 
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
+      resultType: CameraResultType.Uri, // file-based data; provides best performance
+      source: CameraSource.Camera, // automatically take a new photo with the camera
+      quality: 100 // highest quality (0 to 100)
     });
 
     //add newly captured photo to beginning of photos array
@@ -25,7 +65,13 @@ export class PhotoService {
       filepath: "soon...",
       webviewPath: capturedPhoto.webPath
     });
+
+    //save teh picture and add it to photo collection
+    const savedImageFile : UserPhoto = await this.savePicture(capturedPhoto); //implied typing
+    this.photos.unshift(savedImageFile);
   }
+
+  
 }
 
 //outside of photoservice class

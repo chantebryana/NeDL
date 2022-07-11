16c672d04d949c3721f8f29547b27282069573c9
Chante Earthwell <cearthwell@dontpaniclabs.com>
2022-07-11 10:54:23 -0500

    clean up tabs

--- phase 1-3/wk05/tmp/DI/Program.cs
+++ phase 1-3/wk05/tmp/DI/Program.cs
@@ -6,59 +6,59 @@ namespace DI
 {
 
 	class Order 
-{
-    public int Id { get; set; }
-    public decimal Total { get; set; }
-}
+	{
+		public int Id { get; set; }
+		public decimal Total { get; set; }
+	}
 
-class OrderAccessor
-{
-    static Order[] Orders = 
-        new Order[] { new Order() { Id = 1, Total = 100.00M }};
+	class OrderAccessor
+	{
+		static Order[] Orders = 
+			new Order[] { new Order() { Id = 1, Total = 100.00M }};
 
-    public Order Find(int id)
-    {
-        return Orders.First(o => o.Id == id);
-    }
-}
+		public Order Find(int id)
+		{
+			return Orders.First(o => o.Id == id);
+		}
+	}
 
-class EmailAccessor
-{
-    public void SendEmail(Order order)
-    {
-        Console.WriteLine($"Email for {order.Id}");
-    }
-}
+	class EmailAccessor
+	{
+		public void SendEmail(Order order)
+		{
+			Console.WriteLine($"Email for {order.Id}");
+		}
+	}
 
-class OrderManager
-{
+	class OrderManager
+	{
 		private OrderAccessor OrderAccessor { get;set; }
-    private EmailAccessor EmailAccessor { get;set; }
-    public OrderManager(OrderAccessor orderAccessor,      
-        EmailAccessor emailAccessor)
-    {
-        OrderAccessor = orderAccessor;
-        EmailAccessor = emailAccessor;
-    }
+		private EmailAccessor EmailAccessor { get;set; }
+		public OrderManager(OrderAccessor orderAccessor,      
+			EmailAccessor emailAccessor)
+		{
+			OrderAccessor = orderAccessor;
+			EmailAccessor = emailAccessor;
+		}
 
 		public void SendNotification(int id)
-{
-    var order = OrderAccessor.Find(id);
-    EmailAccessor.SendEmail(order);
-}
+		{
+			var order = OrderAccessor.Find(id);
+			EmailAccessor.SendEmail(order);
+		}
 
 
-}
+	}
 
-class Program
-{
-    static void Main(string[] args)
-{
-    var manager = new OrderManager(
-        new OrderAccessor(), new EmailAccessor());
-    manager.SendNotification(1);
-}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var manager = new OrderManager(
+				new OrderAccessor(), new EmailAccessor());
+			manager.SendNotification(1);
+		}
 
-}
+	}
 
 }
